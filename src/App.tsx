ce27170bc9386f12ce88c77fda49581b1530031f
BobbyTheCatfish <46359040+BobbyTheCatfish@users.noreply.github.com>
2025-02-28 23:44:37 -0500

    fix links

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { createTheme, CssBaseline, ThemeProvider, SvgIcon } from '@mui/material';
 import {Discord, Patreon, Twitch} from './assets/svgs'
-import { YouTube, SmartToy, Twitter, ShoppingBag, Plumbing } from '@mui/icons-material'
+import { YouTube, SmartToy, Twitter, Plumbing } from '@mui/icons-material'
 import styled from 'styled-components';
 import CustomButton from './components/button';
 import { Header } from './components/header';
@@ -37,11 +37,11 @@ function App() {
             <CustomButton icon={<YouTube/>} caption="YOUTUBE" color="#FF0000" url="www.youtube.com/channel/UCw8DLllFiJOmevgDznFiQZw"/>
             <CustomButton icon={<SvgIcon><Twitch/></SvgIcon>} caption="TWITCH" color="#6441a5" url="www.twitch.tv/bobbydacatfish"/>
             <CustomButton icon={<SvgIcon><Discord/></SvgIcon>} caption="DISCORD" color="#5865F2" url="www.discord.gg/wj7VVPA"/>
-            <CustomButton icon={<SmartToy/>} caption="BOBBY BOT" color="orange" url="https://github.com/BobbyTheCatfish/bobby-bot"/>
+            <CustomButton icon={<SmartToy/>} caption="BOBBY BOT" color="orange" url="www.github.com/BobbyTheCatfish/bobby-bot"/>
             <CustomButton icon={<Twitter/>} caption="TWITTER" color="#1DA1F2" url="www.twitter.com/bobbythecatfish"/>
             <CustomButton icon={<SvgIcon><Patreon/></SvgIcon>} caption="PATREON" color="#f96854" url="www.patreon.com/bobbythecatfish"/>
-            <CustomButton icon={<ShoppingBag/>} caption="MERCH" color="gray" url="www.teespring.com/stores/bobbys-gift-shop"/>
-            <CustomButton icon={<Plumbing/>} caption="PERSONA MODS" color="gold" url="persona.bobbythecat.fish"/>
+            {/**<CustomButton icon={<ShoppingBag/>} caption="MERCH" color="gray" url="www.teespring.com/stores/bobbys-gift-shop"/>*/}
+            <CustomButton icon={<Plumbing/>} caption="PERSONA MODS" color="gray" url="www.gamebanana.com/members/2378136"/>
           </ButtonGroup>
         </CssBaseline>
       </ThemeProvider>
